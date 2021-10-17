959ab72b66d8852f318b6da035eeb83120d57cfa
Da1v1kd <90835870+Da1v1kd@users.noreply.github.com>
2021-10-17 16:54:30 +0530

    Update Script.js

--- module4-solution/Script.js
+++ module4-solution/Script.js
@@ -1,6 +1,6 @@
 (function () {
 
-var names = ["Mahesh", "Jayesh", "Jenna", "Jack", "BBK", "Luffy", "Zoro", "Sanji", "Nami", "Jennifer"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
